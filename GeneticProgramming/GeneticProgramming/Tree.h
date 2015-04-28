9bfe27f720aa11805e0d8c59e868e50e151cba94
CorcovadoMing <rf37535@gmail.com>
2015-04-29 01:26:57 +0800

    Initialize

---
+++ GeneticProgramming/GeneticProgramming/Tree.h
@@ -0,0 +1,14 @@
+#pragma once
+class Tree
+{
+public:
+	Tree();
+	~Tree();
+private:
+};
+
+class Node
+{
+public:
+private:
+};
---
+++ GeneticProgramming/GeneticProgramming/main.cpp
@@ -0,0 +1,7 @@
+#include "Tree.h"
+
+int main()
+{
+	Tree tree();
+	return 0;
+}
