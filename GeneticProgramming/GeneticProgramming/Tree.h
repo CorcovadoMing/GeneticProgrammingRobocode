86f9c5dda711998509e297da4d2ff79cfe605a5e
CorcovadoMing <rf37535@gmail.com>
2015-05-01 14:00:41 +0800

    Now tree.root() return a pointer to root node

--- GeneticProgramming/GeneticProgramming/Tree.h
+++ GeneticProgramming/GeneticProgramming/Tree.h
@@ -6,8 +6,8 @@ class Tree
 {
 public:
 	Tree(const std::string &);
-	const Node &root() const { return root_; }
-	Node &root() { return root_; }
+	const Node *root() const { return &root_; }
+	Node *root() { return &root_; }
 private:
 	Node root_;
 };
--- GeneticProgramming/GeneticProgramming/main.cpp
+++ GeneticProgramming/GeneticProgramming/main.cpp
@@ -4,14 +4,16 @@
 int main()
 {
 	Tree tree("if");
-	Node* root = &tree.root();
+	Node* root = tree.root();
 	
 	std::cout << root->getData() << " " << root->getType() << std::endl;
 	std::cout << root->numberOfChildren() << std::endl;
 	
 	root->addChild(Node("if"));
+	Node *current_node = root->child(0);
 	std::cout << root->numberOfChildren() << std::endl;
-	std::cout << root->child(0)->getData() << std::endl;
+	std::cout << current_node->getData() << " " << current_node->getType() << std::endl;
+	std::cout << current_node->numberOfChildren() << std::endl;
 
 	return 0;
 }
