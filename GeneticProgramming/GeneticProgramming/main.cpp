5033a412675ce1cc6ea636529dd70e9e73b67bd6
CorcovadoMing <rf37535@gmail.com>
2015-05-02 15:31:52 +0800

    Add some test

--- GeneticProgramming/GeneticProgramming/main.cpp
+++ GeneticProgramming/GeneticProgramming/main.cpp
@@ -8,6 +8,12 @@ int main()
 	for (std::size_t i = 0; i < tree.root()->numberOfChildren(); i += 1)
 	{
 		std::cout << "Child[" << i << "]\t" << tree.root()->child(i)->getType() << std::endl;
+		
+		Node* current = tree.root()->child(i);
+		for (std::size_t j = 0; j < current->numberOfChildren(); j += 1)
+		{
+			std::cout << "Sub-Child[" << j << "]\t" << current->child(j)->getType() << std::endl;
+		}
 	}
 
 	//std::swap(*another_tree_root->child(0), *root->child(0));
