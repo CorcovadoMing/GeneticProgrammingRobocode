ac49cdce308991f27c8750e43fa480fb780716bf
CorcovadoMing <rf37535@gmail.com>
2015-05-08 14:23:27 +0800

    Implement the message queue

--- ControlEngine/main.py
+++ ControlEngine/main.py
@@ -1,15 +1,14 @@
 __author__ = 'Ming'
 
 from shell import *
+import pika
+import time
 
 
 def simulate():
     out, err = shell_command(["D:\\Robocode\\robocode.bat", "-battle", "battle\\intro.battle", "-nodisplay", "-results", "result\\result.txt"])
     print out
-
-    records = parse_result("result\\result.txt")
-    for tank in records:
-        print tank
+    return parse_result("result\\result.txt")
 
 
 def parse_result(filename):
@@ -35,4 +34,22 @@ def parse_result(filename):
     return records
 
 if __name__ == '__main__':
-    simulate()
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='robocode_queue', durable=True)
+    print ' [*] Waiting for messages. To exit press CTRL+C'
+
+    def callback(ch, method, properties, body):
+        print " [x] Received %r" % (body,)
+        if str(body) == "simulate":
+            records = simulate()
+            for tank in records:
+                print tank
+        time.sleep(body.count('.'))
+        print " [x] Done"
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback, queue='robocode_queue')
+    channel.start_consuming()
